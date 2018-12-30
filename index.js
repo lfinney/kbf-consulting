7b07997f62136ec4e5350a91d944af0e93ef230e
Luke Finney <lucaswfinney@gmail.com>
2018-12-29 17:17:33 -0700

    Implement continaer clicking

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 $(document).ready(function(){
-  $('.category').on('click', function() {
-    console.log('hallo!');
-  })
+
+  const selectCategory = (element) => {
+    const notSelectedCateogires = $(element).siblings();
+
+    notSelectedCateogires.each(function(index, element) {
+      const categoryHeading = $(element).children('div.category-heading')
+      const contentContainer = $(element).children('div.content-container')
+      console.log(contentContainer, categoryHeading);
+      $(element).removeClass('none-selected selected').addClass('not-selected');
+      $(categoryHeading).removeClass('selected-heading').addClass('not-selected-heading')
+      $(contentContainer).addClass('hidden')
+    });
+    element.removeClass('none-selected not-selected').addClass('selected');
+  };
+
+
+  const displaySelectedContent = (element) => {
+    const heading = element.find('div.category-heading');
+    const contentContainer = element.find('div.content-container');
+
+    heading.removeClass('not-selected-heading').addClass('selected-heading');
+    contentContainer.removeClass('hidden');
+  };
+
+  $('.category').on('click', function(event) {
+    var element = $(this);
+    selectCategory(element);
+    displaySelectedContent(element);
+  });
 });
