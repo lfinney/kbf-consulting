980b5a22ce6137a377fabd8c80620db168a1ba4a
Luke Finney <lucaswfinney@gmail.com>
2019-02-02 12:45:06 -0700

    Refactor logic for handling tab flow

--- index.js
+++ index.js
@@ -1,66 +1,34 @@
 $(document).ready(function(){
 
-  const getUnselectedTabs = () => {
-    return $('main').find('.tab');
-  }
+  const getTabs = () => $('main').find('.tab');
+
+  const getHeader = () => $('body').find('header');
 
   const manageTabFlow = (element) => {
-    if ($(element).hasClass('selected')) {
-
-    } else if ($(element).hasClass('header-selected')) {
-      $(element).removeClass('header-selected').addClass('header-default')
+    const tabs = getTabs();
+    const header = getHeader();
+    if ($(element).hasClass('tab-selected') || $(element).hasClass('header-selected')) {
+      $(header).removeClass('header-selected header-not-selected').addClass('header-default');
+      $.each(tabs, function(index, tab) {
+        $(tab).removeClass('tab-selected tab-not-selected').addClass('tab-default');
+      });
     } else if ($(element).hasClass('header')) {
-      $(element).removeClass('header-default').addClass('header-selected')
-      const tabs = getUnselectedTabs();
+      $(element).removeClass('header-default header-not-selected').addClass('header-selected')
+      $.each(tabs, function(index, tab) {
+        $(tab).removeClass('tab-selected tab-default').addClass('tab-not-selected');
+      });
     } else if ($(element).hasClass('tab')) {
-      $(element).removeClass('tab-default').addClass('tab-selected')
-      console.log('tab clicked');
+      $.each(tabs, function(index, tab) {
+          $(tab).removeClass('tab-default tab-selected').addClass('tab-not-selected');
+      });
+      $(element).removeClass('tab-not-selected').addClass('tab-selected');
+      $(header).removeClass('header-default').addClass('header-not-selected');
     }
   }
 
-
-
-  const selectCategory = (element) => {
-    const notSelectedCateogires = $(element).siblings();
-
-    notSelectedCateogires.each(function(index, element) {
-      const categoryHeading = $(element).children('div.category-heading')
-      const contentContainer = $(element).children('div.content-container')
-
-      if ($(element).hasClass('selected')) {
-        console.log('has class selected');
-        $(categoryHeading).removeClass('not-selected').addClass('none-selected');
-        // $(categoryHeading).removeClass('selected-heading').addClass('not-selected-heading')
-      } else {
-        $(element).removeClass('none-selected selected').addClass('not-selected');
-        $(categoryHeading).removeClass('selected-heading').addClass('not-selected-heading')
-        $(contentContainer).addClass('hidden')
-      }
-
-    });
-    element.removeClass('none-selected not-selected').addClass('selected');
-  };
-
-
-  const displaySelectedContent = (element) => {
-    const heading = element.find('div.category-heading');
-    const contentContainer = element.find('div.content-container');
-
-    if ($(element).hasClass('selected')) {
-      $(element).removeClass('none-selected selected').addClass('not-selected');
-      heading.removeClass('selected-heading').addClass('not-selected-heading');
-      contentContainer.addClass('hidden');
-      return;
-    }
-
-    heading.removeClass('not-selected-heading').addClass('selected-heading');
-    contentContainer.removeClass('hidden');
-  };
 
   $('.tab, header').on('click', function(event) {
     var element = $(this);
     manageTabFlow(element);
-    // selectCategory(element);
-    // displaySelectedContent(element);
   });
 });
